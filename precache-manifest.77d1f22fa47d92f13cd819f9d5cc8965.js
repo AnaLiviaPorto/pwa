0def0ec66b5353926f2810c2187bc76f216fccc7
Alisson Narjario Queiroga Diniz <alisson.diniz@venturus.org.br>
2019-10-19 19:28:03 -0300

    Updates

---
+++ precache-manifest.77d1f22fa47d92f13cd819f9d5cc8965.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe917b11c22be070cf727f7deb9dd72",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "6da11d56dfcb1e330287",
+    "url": "/pwa/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9ed9f16fec043691949e",
+    "url": "/pwa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6da11d56dfcb1e330287",
+    "url": "/pwa/static/js/2.b788500a.chunk.js"
+  },
+  {
+    "revision": "9ed9f16fec043691949e",
+    "url": "/pwa/static/js/main.f0d4717c.chunk.js"
+  },
+  {
+    "revision": "72328a618a0deb781841",
+    "url": "/pwa/static/js/runtime-main.bf086889.js"
+  },
+  {
+    "revision": "c72ad33ffa6d644194a62f59ecfed656",
+    "url": "/pwa/static/media/light-off.c72ad33f.png"
+  },
+  {
+    "revision": "50bb0c209b269394cc2f05bb43fef774",
+    "url": "/pwa/static/media/light-on.50bb0c20.png"
+  }
+]);
